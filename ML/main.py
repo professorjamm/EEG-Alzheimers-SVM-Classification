a75508ace848f182deee7e4b064fe74f7f29b49e
Anthony Nguyen <147727989+MeeM6336@users.noreply.github.com>
2026-04-06 17:46:32 -0500

    Add files via upload

---
+++ ML/main.py
@@ -0,0 +1,9 @@
+from dataset import preprocess_data
+
+def main():
+  # Dataframe with preprocessed data containing 65 rows
+  preprocessed_df = preprocess_data() # Example usage of the processed dataset
+
+
+if __name__ == "__main__":
+  main()
